ef6ce9bebd4baf893c2792a7aba6bf824c4dce3a
LorenitaTL <lore7096@gmail.com>
2021-06-17 14:15:46 -0500

    First day

---
+++ 30DaysOfJavaScript/Day01-Introduction/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof firstName);
+console.log(typeof isMarried);
+console.log(typeof age);
+console.log(typeof courses);
---
+++ 30DaysOfJavaScript/Day01-Introduction/variable.js
@@ -0,0 +1,33 @@
+let _firstName = "Lorena"; //String
+let _isMarried = true; //Boolean
+let _age; //undefined
+let courses = null; //null
+
+//Declare four variables without assigning values
+let firstaVariable;
+let secondVariable;
+let thirdVariable;
+let fourthVariable;
+
+//Declare four variables with assigned values
+let myName = "Lorena";
+let _myAge = 18;
+let city = "Gómez Palacio";
+let student = false;
+
+let __firstName = "Lorena";
+let __lastName = "Trujillo";
+let __isMarried = true;
+let __country = "Mexico";
+let __age = 24;
+
+let firstName = "Lorena",
+  lastName = "Trujillo",
+  isMarried = true,
+  country = "Mexico";
+
+let myAge = 24;
+let yourAge = 27;
+
+console.log("I am " + myAge + " years old");
+console.log("You are " + yourAge + " years old");
